6fa2558c11738cb2388c38313a4d6c97cebb2167
zao <z454236029@gmail.com>
2017-07-12 17:28:42 +0800

    V 3.1.2

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const DanMuer = require("./src/js/DanMuer.output.js");
+(function(window){
 
-module.exports = [DanMuer];
+let DanMuer = require("./dist/DanMuer-es5.min.js");
+
+window.DanMuer = DanMuer;
+
+if( typeof module != 'undefined' && module.exports ){
+	module.exports = DanMuer;
+} else if( typeof define === "function" && define.amd ){
+	define(function(){ return DanMuer;});
+}
+
+}(window));
